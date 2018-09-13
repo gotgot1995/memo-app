80df850f0cbe932c1e4f08def3f13d14e2e8bb41
Gautier Rouleau <gautier.rouleau@gmail.com>
2018-09-13 12:11:36 +0200

    page draft

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+const LOREM = `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Magnam dicta aperiam tempore aut pariatur eum eius optio modi numquam omnis!`;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  private _list: any[] = [];
+  constructor() {
+    this._list = [
+      {date : '09-09-2018', title : '', color : 'yellow', picture : true, content : LOREM},
+      {date : '10-09-2018', title : '', color : 'yellow', picture : false, content : LOREM},
+      {date : '11-09-2018', title : '', color : 'yellow', picture : false, content : LOREM},
+      {date : '12-09-2018', title : '', color : 'yellow', picture : false, content : LOREM}
+    ];
+  }
 
   ngOnInit() {
+
   }
 
 }
